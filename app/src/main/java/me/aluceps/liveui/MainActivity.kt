479143f493695ba8dbd7aa133a109567c17896c1
aluceps <aluceps@gmail.com>
2023-12-07 16:54:46 +0900

    簡単なレイアウトを追加

--- app/src/main/java/me/aluceps/liveui/MainActivity.kt
+++ app/src/main/java/me/aluceps/liveui/MainActivity.kt
@@ -1,6 +1,7 @@
 package me.aluceps.liveui
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import me.aluceps.liveui.databinding.ActivityMainBinding
 
@@ -10,5 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
